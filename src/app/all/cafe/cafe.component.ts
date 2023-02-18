2e2bf0de067500a7129c479862febcf2964c45da
sree raja krishna <sreerajakrishna2002@gmail.com>
2023-02-18 11:43:01 +0530

    as

--- src/app/all/cafe/cafe.component.ts
+++ src/app/all/cafe/cafe.component.ts
@@ -12,7 +12,21 @@ export class CafeComponent {
     phone:'078453 94944',
       description: ' Chinese · North Indian · Mughlai · Seafood · Desserts · Beverages · Biryani.',
       address:'3rd floor, 4, AA Block 2nd St, Lapis lagoon, AA Block, Shanthi Colony, Anna Nagar, Chennai, Tamil Nadu 600040',
-      image: 'https://im1.dineout.co.in/images/uploads/restaurant/sharpen/1/u/o/p114767-167344617963bec323665e7.jpg?tr=tr:n-medium'
+      image: 'https://b.zmtcdn.com/data/pictures/5/18423075/a8f164ef2abffd826bae9db66db6a75c.png?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A'
+},
+  {
+    title: 'On D Roof Restaurant',
+    phone:'078453 94944',
+      description: ' Chinese · North Indian · Mughlai · Seafood · Desserts · Beverages · Biryani.',
+      address:'3rd floor, 4, AA Block 2nd St, Lapis lagoon, AA Block, Shanthi Colony, Anna Nagar, Chennai, Tamil Nadu 600040',
+      image: 'https://b.zmtcdn.com/data/pictures/chains/2/70422/4bed953ae01aca0e6e8ea5b0ec053ec1.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A'
+},
+  {
+    title: 'On D Roof Restaurant',
+    phone:'078453 94944',
+      description: ' Chinese · North Indian · Mughlai · Seafood · Desserts · Beverages · Biryani.',
+      address:'3rd floor, 4, AA Block 2nd St, Lapis lagoon, AA Block, Shanthi Colony, Anna Nagar, Chennai, Tamil Nadu 600040',
+      image: 'https://b.zmtcdn.com/data/pictures/7/19445787/91bfac31573c91728fd689df06476c32.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A'
 
 },
   {
@@ -20,7 +34,7 @@ export class CafeComponent {
     phone:'078453 94944',
       description: ' Chinese · North Indian · Mughlai · Seafood · Desserts · Beverages · Biryani.',
       address:'3rd floor, 4, AA Block 2nd St, Lapis lagoon, AA Block, Shanthi Colony, Anna Nagar, Chennai, Tamil Nadu 600040',
-      image: 'https://im1.dineout.co.in/images/uploads/restaurant/sharpen/1/u/o/p114767-167344617963bec323665e7.jpg?tr=tr:n-medium'
+      image: 'https://b.zmtcdn.com/data/reviews_photos/657/c42d2d5d59269effcb641a00a9349657_1609858429.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A'
 
 },
   {
@@ -28,7 +42,14 @@ export class CafeComponent {
     phone:'078453 94944',
       description: ' Chinese · North Indian · Mughlai · Seafood · Desserts · Beverages · Biryani.',
       address:'3rd floor, 4, AA Block 2nd St, Lapis lagoon, AA Block, Shanthi Colony, Anna Nagar, Chennai, Tamil Nadu 600040',
-      image: 'https://im1.dineout.co.in/images/uploads/restaurant/sharpen/1/u/o/p114767-167344617963bec323665e7.jpg?tr=tr:n-medium'
+      image: 'https://b.zmtcdn.com/data/pictures/chains/2/65092/ccc705106bf16b321bc3f587ecd6f4f8.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A'
+},
+  {
+    title: 'On D Roof Restaurant',
+    phone:'078453 94944',
+      description: ' Chinese · North Indian · Mughlai · Seafood · Desserts · Beverages · Biryani.',
+      address:'3rd floor, 4, AA Block 2nd St, Lapis lagoon, AA Block, Shanthi Colony, Anna Nagar, Chennai, Tamil Nadu 600040',
+      image: 'https://b.zmtcdn.com/data/reviews_photos/ba5/39ff4516584c9b7f1418f775e21adba5_1477849019.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A'
 
 },
   {
@@ -36,7 +57,14 @@ export class CafeComponent {
     phone:'078453 94944',
       description: ' Chinese · North Indian · Mughlai · Seafood · Desserts · Beverages · Biryani.',
       address:'3rd floor, 4, AA Block 2nd St, Lapis lagoon, AA Block, Shanthi Colony, Anna Nagar, Chennai, Tamil Nadu 600040',
-      image: 'https://im1.dineout.co.in/images/uploads/restaurant/sharpen/1/u/o/p114767-167344617963bec323665e7.jpg?tr=tr:n-medium'
+      image: 'https://b.zmtcdn.com/data/pictures/4/69134/b13e64d471554b72ec20ecf19c2aa4db.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A'
+},
+  {
+    title: 'On D Roof Restaurant',
+    phone:'078453 94944',
+      description: ' Chinese · North Indian · Mughlai · Seafood · Desserts · Beverages · Biryani.',
+      address:'3rd floor, 4, AA Block 2nd St, Lapis lagoon, AA Block, Shanthi Colony, Anna Nagar, Chennai, Tamil Nadu 600040',
+      image: 'https://b.zmtcdn.com/data/reviews_photos/c73/416690e48e2fe1d355d697429018fc73_1615721846.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A'
 
 },
   {
@@ -44,15 +72,7 @@ export class CafeComponent {
     phone:'078453 94944',
       description: ' Chinese · North Indian · Mughlai · Seafood · Desserts · Beverages · Biryani.',
       address:'3rd floor, 4, AA Block 2nd St, Lapis lagoon, AA Block, Shanthi Colony, Anna Nagar, Chennai, Tamil Nadu 600040',
-      image: 'https://im1.dineout.co.in/images/uploads/restaurant/sharpen/1/u/o/p114767-167344617963bec323665e7.jpg?tr=tr:n-medium'
-
-},
-  {
-    title: 'On D Roof Restaurant',
-    phone:'078453 94944',
-      description: ' Chinese · North Indian · Mughlai · Seafood · Desserts · Beverages · Biryani.',
-      address:'3rd floor, 4, AA Block 2nd St, Lapis lagoon, AA Block, Shanthi Colony, Anna Nagar, Chennai, Tamil Nadu 600040',
-      image: 'https://im1.dineout.co.in/images/uploads/restaurant/sharpen/1/u/o/p114767-167344617963bec323665e7.jpg?tr=tr:n-medium'
+      image: 'https://b.zmtcdn.com/data/reviews_photos/5dd/f2a7f80598a7cee7e9a6be432e5e15dd_1557218382.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A'
 
 },
   ]
--- src/app/all/cloth/cloth.component.ts
+++ src/app/all/cloth/cloth.component.ts
@@ -12,7 +12,7 @@ export class ClothComponent {
       phone:'078453 94944',
         description: ' Chinese · North Indian · Mughlai · Seafood · Desserts · Beverages · Biryani.',
         address:'3rd floor, 4, AA Block 2nd St, Lapis lagoon, AA Block, Shanthi Colony, Anna Nagar, Chennai, Tamil Nadu 600040',
-        image: 'https://im1.dineout.co.in/images/uploads/restaurant/sharpen/1/u/o/p114767-167344617963bec323665e7.jpg?tr=tr:n-medium'
+        image: 'https://media.istockphoto.com/id/831656828/photo/luxury-clothing-store-for-men.jpg?s=612x612&w=0&k=20&c=r9vD1Nvv_W0uco8RN7jPKx2HJ7zxj3ISrnbP5XhGszg='
   
   },
     {
@@ -20,7 +20,14 @@ export class ClothComponent {
       phone:'078453 94944',
         description: ' Chinese · North Indian · Mughlai · Seafood · Desserts · Beverages · Biryani.',
         address:'3rd floor, 4, AA Block 2nd St, Lapis lagoon, AA Block, Shanthi Colony, Anna Nagar, Chennai, Tamil Nadu 600040',
-        image: 'https://im1.dineout.co.in/images/uploads/restaurant/sharpen/1/u/o/p114767-167344617963bec323665e7.jpg?tr=tr:n-medium'
+        image: 'https://media.istockphoto.com/id/1023612090/photo/interior-of-clothing-store.jpg?s=612x612&w=0&k=20&c=84NciWwU43Zyzmxph6bCVTG9WRO9rxDGUYtYnUqpTt8='
+  },
+    {
+      title: 'On D Roof Restaurant',
+      phone:'078453 94944',
+        description: ' Chinese · North Indian · Mughlai · Seafood · Desserts · Beverages · Biryani.',
+        address:'3rd floor, 4, AA Block 2nd St, Lapis lagoon, AA Block, Shanthi Colony, Anna Nagar, Chennai, Tamil Nadu 600040',
+        image: 'https://media.istockphoto.com/id/1175412224/photo/woman-buying-clothes-at-department-store-stock-photo.jpg?s=612x612&w=0&k=20&c=A_UJwxOoGXChnbaxgJmY9STnQIyMSJqqBwCrAd_7zsg='
   
   },
     {
@@ -28,7 +35,7 @@ export class ClothComponent {
       phone:'078453 94944',
         description: ' Chinese · North Indian · Mughlai · Seafood · Desserts · Beverages · Biryani.',
         address:'3rd floor, 4, AA Block 2nd St, Lapis lagoon, AA Block, Shanthi Colony, Anna Nagar, Chennai, Tamil Nadu 600040',
-        image: 'https://im1.dineout.co.in/images/uploads/restaurant/sharpen/1/u/o/p114767-167344617963bec323665e7.jpg?tr=tr:n-medium'
+        image: 'https://media.istockphoto.com/id/1434168927/photo/cos-clothing-retail-store.jpg?s=612x612&w=0&k=20&c=9By-IWBJOj2VlcC-SW4GyA5RpUE3qZAU7NfSQ_KuuNw='
   
   },
     {
@@ -36,7 +43,7 @@ export class ClothComponent {
       phone:'078453 94944',
         description: ' Chinese · North Indian · Mughlai · Seafood · Desserts · Beverages · Biryani.',
         address:'3rd floor, 4, AA Block 2nd St, Lapis lagoon, AA Block, Shanthi Colony, Anna Nagar, Chennai, Tamil Nadu 600040',
-        image: 'https://im1.dineout.co.in/images/uploads/restaurant/sharpen/1/u/o/p114767-167344617963bec323665e7.jpg?tr=tr:n-medium'
+        image: 'https://media.istockphoto.com/id/518733931/photo/interior-of-shoe-store-in-modern-european-mall.jpg?s=612x612&w=0&k=20&c=rU_T0Q8TqNLY-WJHYjBieHk26AhLwwQWJe_2hNOI_eo='
   
   },
     {
@@ -44,16 +51,7 @@ export class ClothComponent {
       phone:'078453 94944',
         description: ' Chinese · North Indian · Mughlai · Seafood · Desserts · Beverages · Biryani.',
         address:'3rd floor, 4, AA Block 2nd St, Lapis lagoon, AA Block, Shanthi Colony, Anna Nagar, Chennai, Tamil Nadu 600040',
-        image: 'https://im1.dineout.co.in/images/uploads/restaurant/sharpen/1/u/o/p114767-167344617963bec323665e7.jpg?tr=tr:n-medium'
-  
-  },
-    {
-      title: 'On D Roof Restaurant',
-      phone:'078453 94944',
-        description: ' Chinese · North Indian · Mughlai · Seafood · Desserts · Beverages · Biryani.',
-        address:'3rd floor, 4, AA Block 2nd St, Lapis lagoon, AA Block, Shanthi Colony, Anna Nagar, Chennai, Tamil Nadu 600040',
-        image: 'https://im1.dineout.co.in/images/uploads/restaurant/sharpen/1/u/o/p114767-167344617963bec323665e7.jpg?tr=tr:n-medium'
-  
+        image: 'https://media.istockphoto.com/id/182408547/photo/elegant-shopping-mall.jpg?s=612x612&w=0&k=20&c=qpoZ75-JlU67tx710rgzp4Eb38kb5H-2M_bG9TTEZK0='
   },
 ]
 }
