468cb72082d1a34da2b191391e99f26dbc28d1ad
sree raja krishna <sreerajakrishna2002@gmail.com>
2023-02-20 10:26:04 +0530

    1

--- src/app/things/things.component.ts
+++ src/app/things/things.component.ts
@@ -6,5 +6,74 @@ import { Component } from '@angular/core';
   styleUrls: ['./things.component.css']
 })
 export class ThingsComponent {
+  explores=[
+    {
+      title:'Art and activities',
+      area:'Mylapore',
+      area2:'George Town',
+      area3:'Royapetah'
+    },
+    {
+      title:'Street Shopping:',
+      area:'Pondy Bazzar',
+      area2:'Pantheon road',
+      area3:'Moore street',
+      area4:'Ranganathan street'
+    },
+    {
+      title:'Library',
+      area:'Connemara',
+      area2:'Anna Cenetenary Library',
+    },
+    {
+      title:'Malls',
+      area:'VR Mall',
+      area2:'Express Avenue',
+      area3:'Phoneix Market City'
+    },
+    {
+      title:'Art and activities',
+      area:'Mylapore',
+      area2:'George Town',
+      area3:'Royapetah'
+    },
+    {
+      title:'Art and activities',
+      area:'Mylapore',
+      area2:'George Town',
+      area3:'Royapetah'
+    },
+    {
+      title:'Religious Places',
+      area:'',
+      area2:'George Town',
+      area3:'Royapetah'
+    },
+    {
+      title:'Beaches',
+      area:'Marina Mall',
+      area2:'kovalam',
+      area3:'Aakkarai Beach',
+      area4:'Eliot Beach',
+      area5:'Thiruvanmiyur Beach'
+
+    },
+    {
+      title:'Mini Gateways',
+      area:'Pulicat Lake',
+      area2:'Mahabalipuram',
+      area3:'Pondicherry',
+      area4:'Yelagiri',
+      area6:'Yercaud',
+      area5:'Vellore'
+    },
+    {
+      title:'Staycation',
+      area:'Raintree',
+      area2:'Taj Mews',
+      area3:'Somerset',
+      area4:'St.Mary \'s Road',
+    }
+  ]
 
 }
