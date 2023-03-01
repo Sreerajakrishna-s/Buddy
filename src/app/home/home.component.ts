7b8cc64a90cd35a28c5c5a253b46c60ddf6bfd05
sree raja krishna <sreerajakrishna2002@gmail.com>
2023-03-01 10:15:42 +0530

    commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,31 +1,3 @@
-// import { Component ,ViewChild, ElementRef,  NgZone  } from '@angular/core';
-
-// @Component({
-//   selector: 'app-home',
-//   templateUrl: './home.component.html',
-//   styleUrls: ['./home.component.css']
-// })
-// export class HomeComponent {
-//   @ViewChild('searchBar', {static: false}) searchBar: ElementRef;
-
-//   searchText = 'Type your search text here';
-
-//   constructor(private ngZone: NgZone) {}
-
-//   autoType() {
-//     // Get the input element and set its value to the searchText variable
-//     const inputElement = this.searchBar.nativeElement;
-//     inputElement.value = this.searchText;
-
-//     // Trigger the keyup event
-//     const event = new KeyboardEvent('keyup', { key: 'Enter' });
-
-//     // Use ngZone to run the event dispatching code outside of the Angular zone
-//     this.ngZone.run(() => {
-//       inputElement.dispatchEvent(event);
-//     });
-//   }
-// }
 import { Component } from '@angular/core';
 
 @Component({
@@ -34,7 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  
+   pop:boolean=false;
+  add:boolean=false;
+  addfu(){
+    this.add=true;
+    this.pop=true;
+  }
   searchQuery = '';
 
   constructor() { }
@@ -59,4 +36,3 @@ export class HomeComponent {
     }, 100);
   }
 }
-
